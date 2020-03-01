2a1f62d61d001129c51d0f43fd1f48ff848856e7
KalilDev <kalilgamer0@gmail.com>
2020-03-01 13:52:47 -0300

    isolate_wrapper: Move outside of cartesian_isolate

--- lib/cartesian_isolate.dart
+++ lib/cartesian_isolate.dart
@@ -1,81 +1,9 @@
-import 'dart:async';
 import 'dart:isolate';
 import 'dart:typed_data';
 import 'package:tuple/tuple.dart';
 import 'package:meta/meta.dart';
 import 'package:flutter/material.dart';
 import 'package:image/image.dart' as img;
-
-/*typedef Result IsolateFunction<Result, Message>(Message msg);
-
-void isolateFunctionWrapper(SendPort mainSink) {
-  final ReceivePort isolateStream = ReceivePort();
-  mainSink.send(isolateStream.sendPort);
-
-  isolateStream.listen((dynamic message) {
-    final data = message as Tuple2;
-    final IsolateFunction F = data.item1;
-    final msg = data.item2;
-    final result = F(msg);
-    mainSink.send(result);
-  });
-}*/
-
-Future<Result> runOnIsolate<Result, Message>(
-    void F(SendPort s), Message msg) async {
-  // Setup communication with isolate
-  final ReceivePort mainStream = ReceivePort();
-  final ReceivePort errorStream = ReceivePort();
-  final ReceivePort exitStream = ReceivePort();
-  // Start isolate with F. Sidenote: F needs to be static and send the isolate's
-  // sendPort through the main sink. We cannot wrap F because dart isolates can't
-  // receive functions (even static ones), so the user MUSTN't fuck this up.
-  final Isolate isolate = await Isolate.spawn<SendPort>(F, mainStream.sendPort,
-      onError: errorStream.sendPort, onExit: exitStream.sendPort);
-
-  void dispose() {
-    isolate?.kill(priority: Isolate.immediate);
-    mainStream.close();
-    errorStream.close();
-    exitStream.close();
-  }
-
-  final Completer<Result> completer = Completer<Result>();
-
-  mainStream.listen((data) {
-    if (data is SendPort) {
-      // This is the first message, and it ALWAYS should be the isolate's
-      // [SendPort]. Be sure that F sends it!!!!
-      data.send(msg);
-    } else {
-      // This is the actual calculation returned from the isolate, it should be
-      // of the return type.
-      try {
-        final Result result = data as Result;
-        completer.complete(result);
-      } on CastError catch (e, s) {
-        // The stack trace has no relevant info in this case.
-        completer.completeError(e, s);
-      } finally {
-        dispose();
-      }
-    }
-  });
-  errorStream.listen((e) {
-    completer.completeError(e);
-    dispose();
-  });
-  exitStream.listen((e) {
-    if (!completer.isCompleted) {
-      // This shoudn't have happened!
-      completer.completeError(Exception(
-          'The isolate was killed before it completed the calculation!'));
-      dispose();
-    }
-  });
-
-  return completer.future;
-}
 
 @immutable
 class PixelDataMessage {
